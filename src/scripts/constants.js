4f0e2c5ae7a5071a67dff16e3936b730686d4a6c
Ivan2399 <Safsoft2399@gmail.com>
2022-05-01 17:16:06 +0300

    fix

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,70 +1,130 @@
-export const cards = document.querySelector(".elements");
-// any
-export const profile = document.querySelector(".profile");
-export const profileName = profile.querySelector(".profile__name");
-export const hobby = profile.querySelector(".profile__hobby");
-export const avatar = profile.querySelector(".profile__avatar");
-// buttons
-export const addButton = profile.querySelector(".profile__button-add");
-export const avatarEditButton = profile.querySelector(
-  ".profile__avatar-button-edit"
-);
-export const editButton = profile.querySelector(".profile__button-edit");
-export const exitButtons = document.querySelectorAll(".popup__button-exit");
-// popup
-export const popupOverlays = document.querySelectorAll(".popup");
-export const popupAvatarEdit = document.querySelector("#popup-avatar-edit");
-export const popupEdit = document.querySelector("#popup-edit");
-export const popupAdd = document.querySelector("#popup-add");
-export const popupOpenImage = document.querySelector("#popup-image");
-export const popupImage = popupOpenImage.querySelector(".popup__image");
-export const popupImageTitle = popupOpenImage.querySelector(
-  ".popup__image-title"
-);
-// inputs
-export const linkInputOfAvatar = popupAvatarEdit.querySelector(
-  ".popup__input-item_type_link"
-);
-export const nameInput = formEditProfileElement.querySelector(
-  ".popup__input-item_type_name"
-);
-export const jobInput = formEditProfileElement.querySelector(
-  ".popup__input-item_type_profession"
-);
-export const cardNameInput = formAddElement.querySelector(
-  ".popup__input-item_type_card-name"
-);
-export const linkInput = formAddElement.querySelector(
-  ".popup__input-item_type_link"
-);
-// forms
-export const formAvatarEdit = popupAvatarEdit.querySelector(".popup__form");
-export const formEditProfileElement = popupEdit.querySelector(".popup__form");
-export const formAddElement = popupAdd.querySelector(".popup__form");
+export const cards = document.querySelector('.elements');
 
+export const profile = document.querySelector('.profile');
+export const profileName = profile.querySelector('.profile__name');
+export const hobby = profile.querySelector('.profile__hobby');
+export const editButton = profile.querySelector('.profile__button-edit');
+export const addButton = profile.querySelector('.profile__button-add');
+export const avatar = profile.querySelector('.profile__avatar');
+export const avatarEditButton = profile.querySelector('.profile__avatar-button-edit');
+
+export const popupOverlays = document.querySelectorAll('.popup');
+
+export const popupAvatarEdit = document.querySelector('#popup-avatar-edit');
+export const formAvatarEdit =  popupAvatarEdit.querySelector('.popup__form');
+export const linkInputOfAvatar = popupAvatarEdit.querySelector('.popup__input-item_type_link');
+
+export const popupEdit = document.querySelector('#popup-edit');
+export const formEditProfileElement =  popupEdit.querySelector('.popup__form');
+export const nameInput = formEditProfileElement.querySelector('.popup__input-item_type_name');
+export const jobInput = formEditProfileElement.querySelector('.popup__input-item_type_profession');
+
+export const popupAdd = document.querySelector('#popup-add');
+export const formAddElement =  popupAdd.querySelector('.popup__form');
+export const cardNameInput = formAddElement.querySelector('.popup__input-item_type_card-name');
+export const linkInput = formAddElement.querySelector('.popup__input-item_type_link');
+
+export const popupOpenImage = document.querySelector('#popup-image');
+export const popupImage =  popupOpenImage.querySelector('.popup__image');
+export const popupImageTitle =  popupOpenImage.querySelector('.popup__image-title');
+
+export const exitButtons = document.querySelectorAll('.popup__button-exit');
+
+// cards
 export const initialCards = [
   {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
 ];
+
+// export const cards = document.querySelector(".elements");
+// // any
+// export const profile = document.querySelector(".profile");
+// export const exitButtons = document.querySelectorAll(".popup__button-exit");
+// export const popupOpenImage = document.querySelector("#popup-image");
+// export const popupAdd = document.querySelector("#popup-add");
+// export const popupEdit = document.querySelector("#popup-edit");
+// export const popupAvatarEdit = document.querySelector("#popup-avatar-edit");
+// export const popupOverlays = document.querySelectorAll(".popup");
+// export const profileName = profile.querySelector(".profile__name");
+// export const hobby = profile.querySelector(".profile__hobby");
+// export const avatar = profile.querySelector(".profile__avatar");
+// // buttons
+// export const addButton = profile.querySelector(".profile__button-add");
+// export const avatarEditButton = profile.querySelector(
+//   ".profile__avatar-button-edit"
+// );
+// export const editButton = profile.querySelector(".profile__button-edit");
+// // popup
+// export const popupImage = popupOpenImage.querySelector(".popup__image");
+// export const popupImageTitle = popupOpenImage.querySelector(
+//   ".popup__image-title"
+// );
+// // inputs
+// export const linkInputOfAvatar = popupAvatarEdit.querySelector(
+//   ".popup__input-item_type_link"
+// );
+// export const formEditProfileElement = popupEdit.querySelector(".popup__form");
+// export const nameInput = formEditProfileElement.querySelector(
+//   ".popup__input-item_type_name"
+// );
+// export const jobInput = formEditProfileElement.querySelector(
+//   ".popup__input-item_type_profession"
+// );
+// export const cardNameInput = formAddElement.querySelector(
+//   ".popup__input-item_type_card-name"
+// );
+// export const linkInput = formAddElement.querySelector(
+//   ".popup__input-item_type_link"
+// );
+// // forms
+// export const formAvatarEdit = popupAvatarEdit.querySelector(".popup__form");
+// export const formAddElement = popupAdd.querySelector(".popup__form");
+
+// export const initialCards = [
+//   {
+//     name: "Архыз",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+//   },
+//   {
+//     name: "Челябинская область",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+//   },
+//   {
+//     name: "Иваново",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+//   },
+//   {
+//     name: "Камчатка",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+//   },
+//   {
+//     name: "Холмогорский район",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+//   },
+//   {
+//     name: "Байкал",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+//   },
+// ];
