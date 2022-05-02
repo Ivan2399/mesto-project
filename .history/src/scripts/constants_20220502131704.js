c405f2d4a1d1181f2debe036cd80bfda050aaaed
Ivan2399 <Safsoft2399@gmail.com>
2022-05-02 13:23:14 +0300

    fix

--- .history/src/scripts/constants_20220502131704.js
+++
@@ -1,4 +0,0 @@
-const cardTemplate = document.querySelector('#card').content;
-const popupOpenImage = document.querySelector('#popup-image');
-const popupImage =  popupOpenImage.querySelector('.popup__image');
-const popupImageTitle =  popupOpenImage.querySelector('.popup__image-title');
