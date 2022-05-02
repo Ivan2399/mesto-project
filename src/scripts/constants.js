1b12c697af33b830ca2a8a4e59ee4c9bcb6b35e7
Ivan2399 <Safsoft2399@gmail.com>
2022-05-02 13:42:03 +0300

    fix

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,3 +1,7 @@
+export const cardTemplate = document.querySelector('#card').content;
+export const popupOpenImage = document.querySelector('#popup-image');
+export const popupImage =  popupOpenImage.querySelector('.popup__image');
+export const popupImageTitle =  popupOpenImage.querySelector('.popup__image-title');
 export const cards = document.querySelector('.elements');
 
 export const profile = document.querySelector('.profile');
@@ -15,46 +19,13 @@ export const formAvatarEdit =  popupAvatarEdit.querySelector('.popup__form');
 export const linkInputOfAvatar = popupAvatarEdit.querySelector('.popup__input-item_type_link');
 
 export const popupEdit = document.querySelector('#popup-edit');
-export const formEditProfileElement =  popupEdit.querySelector('.popup__form');
-export const nameInput = formEditProfileElement.querySelector('.popup__input-item_type_name');
-export const jobInput = formEditProfileElement.querySelector('.popup__input-item_type_profession');
+export const profileForm =  popupEdit.querySelector('.popup__form');
+export const nameInput = profileForm.querySelector('.popup__input-item_type_name');
+export const jobInput = profileForm.querySelector('.popup__input-item_type_profession');
 
 export const popupAdd = document.querySelector('#popup-add');
 export const formAddElement =  popupAdd.querySelector('.popup__form');
 export const cardNameInput = formAddElement.querySelector('.popup__input-item_type_card-name');
 export const linkInput = formAddElement.querySelector('.popup__input-item_type_link');
 
-export const popupOpenImage = document.querySelector('#popup-image');
-export const popupImage =  popupOpenImage.querySelector('.popup__image');
-export const popupImageTitle =  popupOpenImage.querySelector('.popup__image-title');
-
 export const exitButtons = document.querySelectorAll('.popup__button-exit');
-
-// cards
-export const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
